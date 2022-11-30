c751def9e3ded8906ee29319c4fef09527b949e3
Christin Schade <christinabbe90gmail.com>
2022-11-30 15:59:29 +0100

    dotenv

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 import pkg from 'pg';
 const { Pool } = pkg;
+import dotenv from "dotenv"
+dotenv.config();
+
+console.log("env : ", process.env.MY_URL)
 
 const myConnectionString = 'postgres://comeewlw:ol9onWkfIsIuzWUk26Sx--Rx2HTiSjpE@mouse.db.elephantsql.com/comeewlw';
 
